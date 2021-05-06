5cea02730f5c2113af456e076ee27c3bf51e0a50
Firna Firdiani <70745005+vyrnvyrd@users.noreply.github.com>
2021-05-06 19:07:49 +0700

    feature(restaurant-info-card): add open and close

--- App.js
+++ App.js
@@ -2,10 +2,25 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/insfrastructure/theme/index";
-
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
 
 export default function App() {
+  const [oswaldLoaeded] = useOswald({
+    Oswald_400Regular,
+  });
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaeded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>

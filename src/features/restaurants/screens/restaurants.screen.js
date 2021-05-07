286970d6092e03c9da26e474229f73e354f0abe3
Firna Firdiani <70745005+vyrnvyrd@users.noreply.github.com>
2021-05-07 11:45:24 +0700

    feature(restaurant): add list to restaurant

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { FlatList } from "react-native";
 import styled from "styled-components";
 import { Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { DroidSafeArea } from "../../../utils/global";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
 const SafeArea = DroidSafeArea;
 
@@ -10,19 +12,25 @@ const SearchView = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const ListView = styled.View`
-  flex: 1;
-  background-color: ${(props) => props.theme.colors.bg.secondary};
-  padding: ${(props) => props.theme.space[3]};
-`;
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
 
 export const RestaurantsScreen = () => (
   <SafeArea>
     <SearchView>
       <Searchbar />
     </SearchView>
-    <ListView>
-      <RestaurantInfoCard />
-    </ListView>
+    <RestaurantList
+      data={[{ name: 1 }, { name: 2 }, { name: 3 }]}
+      renderItem={() => (
+        <Spacer position="bottom" size="large">
+          <RestaurantInfoCard />
+        </Spacer>
+      )}
+      keyExtractor={(item) => item.name}
+    />
   </SafeArea>
 );

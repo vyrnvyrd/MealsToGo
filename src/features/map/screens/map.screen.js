e32a384e4e2bd07d826a97558fe0bab35345e884
Firna Firdiani <70745005+vyrnvyrd@users.noreply.github.com>
2021-05-10 15:46:48 +0700

    feature(map): add navigation to detail restaurant

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -11,7 +11,7 @@ const Map = styled(MapView)`
   width: 100%;
 `;
 
-export const MapScreen = () => {
+export const MapScreen = ({ navigation }) => {
   const { location } = useContext(LocationContext);
   const { restaurants = [] } = useContext(RestaurantsContext);
 
@@ -46,7 +46,13 @@ export const MapScreen = () => {
                 longitude: restaurant.geometry.location.lng,
               }}
             >
-              <MapView.Callout>
+              <MapView.Callout
+                onPress={() =>
+                  navigation.navigate("RestaurantDetail", {
+                    restaurant,
+                  })
+                }
+              >
                 <MapCallout restaurant={restaurant} />
               </MapView.Callout>
             </MapView.Marker>
